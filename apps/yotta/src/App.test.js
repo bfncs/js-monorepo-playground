a40624a674ca2964e9ecc01d29cd71503615e248
Marc Löhe <marc@marcloehe.de>
2017-09-28 16:33:43 +0200

    Add snapshot test to yotta app

--- apps/yotta/src/App.test.js
+++ apps/yotta/src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+
+it('renders correctly', () => {
+  expect(renderer.create(<App />).toJSON()).toMatchSnapshot();
 });
